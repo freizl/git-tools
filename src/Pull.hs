eafc90a04f5cf9506c334c6f7ed6005ce36aea0c
Haisheng.Wu.Okta <freizl@gmail.com>
2017-07-24 10:20:16 -0700

    ignore error.

--- src/Pull.hs
+++ src/Pull.hs
@@ -5,11 +5,11 @@ module Pull where
 import qualified Data.Text                 as Text
 import           System.Directory
 import qualified Turtle.Prelude            as T
---import qualified Turtle.Shell         as T
 import           Control.Monad             (when)
 import           Control.Monad.Managed     (with)
 import qualified Filesystem.Path           as Path
 import qualified Filesystem.Path.CurrentOS as Path
+import Control.Exception
 
 dirs :: IO [FilePath]
 dirs = getCurrentDirectory >>= listDirectory
@@ -17,14 +17,15 @@ dirs = getCurrentDirectory >>= listDirectory
 gitpull :: FilePath -> IO ()
 gitpull f = do
   c <- getCurrentDirectory
-  ds <- listDirectory (c ++ f)
+  ds <- listDirectory (c ++ "/" ++ f)
   let isGitRepo = ".git" `elem` ds
   when isGitRepo $
-    with (T.pushd (toDir c `Path.append` toDir f)) runUpdate
+    with (T.pushd (toDir c `Path.append` toDir f)) runUpdate `catch` (\e -> print (e :: T.ShellFailed))
 
 runUpdate :: () -> IO ()
 runUpdate _ = do
-  T.pwd >>= print
+  c <- T.pwd
+  putStr ">> " >> print c
   T.shells "git stash" T.stdin
   T.shells "git pull" T.stdin
 
